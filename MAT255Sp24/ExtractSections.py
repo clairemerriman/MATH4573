b90f871c419a947ab273dad549460f094b0413f0
clairemerriman <16827321+clairemerriman@users.noreply.github.com>
2024-04-04 00:06:39 -0400

    Create ExtractSections.py
    Takes the sections from my weekly course notes and creates individual files in the properly named folder

---
+++ MAT255Sp24/ExtractSections.py
@@ -0,0 +1,52 @@
+import re
+import os
+
+def extract_all_sections(input_file):
+
+    # Use input file name to create the folder for the week
+    filename = os.path.splitext(os.path.basename(input_file))[0]
+    folder_name = filename.split('Sp')[0]
+
+    # Create folder for output files
+    os.makedirs(folder_name, exist_ok=True)
+    with open(input_file, 'r') as f:
+        lines = f.readlines()
+
+    append_line = False
+    extracted_lines = []
+
+    for line in lines:
+        if append_line:
+            if line.startswith(r'\section') or line.startswith(r'\end{document}'):
+                output_content = r'''\documentclass{../ximera}
+\input{../lessonplanheader.tex}
+
+\title{''' + section_title + r'''}
+\begin{document}
+\begin{abstract}
+\end{abstract}
+\maketitle
+
+''' + ''.join(extracted_lines) + r'''
+
+\end{document}
+'''
+                #Now we use the content information from the section title to name the file
+                #Remove spaces and special characters
+
+                file_title = ''.join(letter for letter in section_title if letter.isalnum())
+
+                output_file = os.path.join(folder_name, f"{file_title.strip()}.tex")
+
+                with open(output_file, "w") as section_file:
+                    section_file.write(output_content)
+                break
+            else:
+                extracted_lines.append(line)
+        elif line.startswith(r'\section') or line.startswith(r'\section*'):
+            append_line = True
+            section_title = line.split(':', 1)[1].split('}')[0]
+            # extracted_lines.append(line)
+
+
+sections = extract_all_sections('Week1Spring24.tex')
